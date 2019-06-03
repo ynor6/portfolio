106adea02997516626a54a7c1ca97586d91d4841
Rony Hirsch <ynor6@outlook.com>
2019-06-03 23:45:20 +0300

    Changed navbar color when scrolling created title and p style to the project page

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-window.addEventListener("keydown", function(e) {
-    // space and arrow keys
-    if (scrollY <=0 && [32, 37, 38, 39, 40].indexOf(e.keyCode) > -1 ){
-        e.preventDefault();
-    }
-}, false);
 
+const initUpdateNavbarOnScroll = () => {
+    const navbar = document.querySelector('.container-1');
+    const elements = document.querySelectorAll('a');
+    if (navbar) {
+        window.addEventListener('scroll', () => {
+        if (window.scrollY >= window.innerHeight-navbar.offsetHeight) {
+            navbar.classList.add('new-class');
+            [].slice.call(elements).forEach(function(elem) {
+                elem.style.color = "rgb(59, 67, 70)";
+                elem.classList.add('blue');
+            });
+        } else {
+            navbar.classList.remove('new-class');
+            [].slice.call(elements).forEach(function(elem) {
+                elem.style.color = "rgb(255, 216, 87)";
+                elem.classList.add('yellow');
+                elem.classList.remove('blue');
+            });
+        }
+        });
+    }
+}
+
+const initScrolling = () => {
+    window.addEventListener("keydown", function(e) {
+        // space and arrow keys
+        if (scrollY <=0 && [32, 37, 38, 39, 40].indexOf(e.keyCode) > -1 ){
+            e.preventDefault();
+        }
+    }, false);
+}
+const initPage = () => {
+    initUpdateNavbarOnScroll();
+    initScrolling();
+}
